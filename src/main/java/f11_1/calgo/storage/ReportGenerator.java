d7c7d1b5447c24b02b50c54bb7f401c62a608db4
buddhavineeth <buddhavineeth@gmail.com>
2020-03-17 13:57:39 +0800

    Modularise ReportGenerator functionality

--- src/main/java/f11_1/calgo/storage/ReportGenerator.java
+++ src/main/java/f11_1/calgo/storage/ReportGenerator.java
@@ -17,12 +17,16 @@ import java.util.logging.Logger;
  */
 public class ReportGenerator {
     private static final Logger logger = LogsCenter.getLogger(ReportGenerator.class);
-    private DailyFoodLog foodLog;
+    private Day queryDay;
     private File file;
     private PrintWriter printWriter;
+    private double totalCalories = 0.0;
+    private double totalProteins = 0.0;
+    private double totalCarbs = 0.0;
+    private double totalFats = 0.0;
 
     public ReportGenerator(Day queryDay) {
-        this.foodLog = queryDay.getDailyFoodLog();
+        this.queryDay = queryDay;
         this.file = new File(queryDay.toString() + "_report.txt");
         try {
             this.printWriter = new PrintWriter(file);
@@ -35,13 +39,27 @@ public class ReportGenerator {
     }
 
     public boolean generateReport() {
-        double totalCalories = 0;
-        double totalProteins = 0;
-        double totalCarbs = 0;
-        double totalFats = 0;
-        printWriter.println(String.format("%-20s %-20s %-20s", "--------------", "--------------", "--------------"));
+        printHeader();
+        printSeparator();
+        printFoodwiseStatistics();
+        printSeparator();
+        printAggregateStatistics();
+        printSeparator();
+        printFooter();
+        printWriter.close();
+        return file.exists() && (file.length() != 0); // success check
+    }
+
+
+    public void printHeader() {
+        String title = "Report of Consumption Pattern on " + this.queryDay.getLocalDate().toString();
+        printWriter.println(title);
+    }
+
+    public void printFoodwiseStatistics() {
+        printWriter.println("Food-wise Statistics:");
         printWriter.println(String.format("%-20s %-20s %-20s", "    Food", "   Quantity", "   Calories"));
-        printWriter.println(String.format("%-20s %-20s %-20s", "--------------", "--------------", "--------------"));
+        DailyFoodLog foodLog = queryDay.getDailyFoodLog();
         for (Food food : foodLog.getFoods().keySet()) {
             double portion = foodLog.getPortion(food);
             double currCalories = portion * (double) Integer.parseInt(food.getCalorie().value);
@@ -51,12 +69,22 @@ public class ReportGenerator {
             totalFats += portion * (double) Integer.parseInt(food.getFat().value);
             printWriter.println(String.format("%-20s %-20f %-20f", food.toString(), portion, currCalories));
         }
-        printWriter.println(String.format("%-20s %-20s %-20s", "--------------", "--------------", "--------------"));
+    }
+
+    public void printAggregateStatistics() {
         printWriter.println("Aggregate Statistics:");
         printWriter.println(String.format("%s %-20s %-20s %-20s", "Total Calories in kcal", "| Total Protein in grams"
                 , "| Total Carbohydrates in grams", "| Total Fats in grams"));
-        printWriter.println(String.format("     %-25f %-26f %-28f %f", totalCalories, totalProteins, totalCarbs, totalFats));
-        printWriter.close();
-        return file.exists() && (file.length() != 0); // success check
+        printWriter.println(String.format("     %-25f %-26f %-28f %f", totalCalories, totalProteins,
+                totalCarbs, totalFats));
+    }
+
+    public void printFooter() {
+        printWriter.println("This marks the end of your report. More insights coming up in v1.3.");
+    }
+
+    public void printSeparator() {
+        printWriter.println("--------------------------------------------------------------------------------"
+                + "-----------------------------");
     }
 }
