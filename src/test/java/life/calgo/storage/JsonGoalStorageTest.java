05120137534c789a86f6cd65ac9de28bbe9f9e0a
buddhavineeth <buddhavineeth@gmail.com>
2020-04-13 14:38:39 +0800

    fix test

--- src/test/java/life/calgo/storage/JsonGoalStorageTest.java
+++ src/test/java/life/calgo/storage/JsonGoalStorageTest.java
@@ -21,7 +21,6 @@ public class JsonGoalStorageTest {
     @TempDir
     public Path testFolder;
 
-
     // Tests for Reading
 
     // test if JsonGoalStorage reads from null file path.
@@ -48,37 +47,39 @@ public class JsonGoalStorageTest {
         Assert.assertThrows(DataConversionException.class, () -> readGoal("invalidGoal.json"));
     }
 
-    @Test
-    public void readAndSaveGoal_allInOrder_success() throws Exception {
-        Path filePath = testFolder.resolve("TempGoal.json");
-        DailyGoal original = new DailyGoal(DailyGoal.MINIMUM_HEALTHY_CALORIES);
-        JsonGoalStorage jsonGoalStorage = new JsonGoalStorage(filePath);
-
-        // Save in new file and read back without any loss or change of data
-        jsonGoalStorage.saveGoal(original, filePath);
-        ReadOnlyGoal readBack = jsonGoalStorage.readGoal(filePath).get();
-        assertEquals(original, new DailyGoal(readBack));
-
-        // Modify data, overwrite exiting file, and read back
-        original = original.updateDailyGoal(DailyGoal.MAXIMUM_ACCEPTABLE_CALORIES);
-        jsonGoalStorage.saveGoal(original, filePath);
-        readBack = jsonGoalStorage.readGoal(filePath).get();
-        assertEquals(original, new DailyGoal(readBack));
-
-        // Save and read without specifying file path
-        original = original.updateDailyGoal(DailyGoal.MINIMUM_ACCEPTABLE_CALORIES);
-        jsonGoalStorage.saveGoal(original); // file path not specified
-        readBack = jsonGoalStorage.readGoal().get(); // file path not specified
-        assertEquals(original, new DailyGoal(readBack));
-
-    }
-
     // Tests for Saving
 
     // test if JsonGoalStorage will save a Null Daily Goal.
     @Test
     public void saveDailyGoal_nullDailyGoal_throwsNullPointerException() {
         Assert.assertThrows(NullPointerException.class, () -> saveGoal(null, "AnyFile.json"));
+    }
+
+    // tests for Reading and Saving
+
+    @Test
+    public void readAndSaveGoal_allInOrder_success() throws Exception {
+        Path filePath = testFolder.resolve("TempGoal.json");
+        DailyGoal testGoal = new DailyGoal(DailyGoal.MINIMUM_HEALTHY_CALORIES);
+        JsonGoalStorage jsonGoalStorage = new JsonGoalStorage(filePath);
+
+        // Save in new file and read back without any loss or change of data
+        jsonGoalStorage.saveGoal(testGoal, filePath);
+        ReadOnlyGoal readBackGoal = jsonGoalStorage.readGoal(filePath).get();
+        assertEquals(testGoal, new DailyGoal(readBackGoal));
+
+        // Modify data, overwrite exiting file, and read back
+        testGoal = testGoal.updateDailyGoal(DailyGoal.MAXIMUM_ACCEPTABLE_CALORIES);
+        jsonGoalStorage.saveGoal(testGoal, filePath);
+        readBackGoal = jsonGoalStorage.readGoal(filePath).get();
+        assertEquals(testGoal, new DailyGoal(readBackGoal));
+
+        // Save and read without specifying file path
+        testGoal = testGoal.updateDailyGoal(DailyGoal.MINIMUM_ACCEPTABLE_CALORIES);
+        jsonGoalStorage.saveGoal(testGoal); // file path not specified
+        readBackGoal = jsonGoalStorage.readGoal().get(); // file path not specified
+        assertEquals(testGoal, new DailyGoal(readBackGoal));
+
     }
 
     // test if JsonGoalStorage will save a valid Goal at a null file path.
